7387ee1f676c1f9df5840bc25ac40a25bf87b343
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-15 15:47:48 +1300

    started writing code

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './test.css';
+
+class Test extends Component {
+    render(){
+        return(
+            <div>
+                <p>I like pineapples</p>
+            </div>
+        )
+    }
+}
+
+export default Test;
