afda34af2818c930f3f478cfa660880a823da038
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-22 15:44:36 +1300

    add item buttons finished

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './Form.css';
+import Button from '../Button/Button';
 
 class Form extends Component {
     render(){
         return(
-            <form>
-
+            <form className="searchForm">
+                <input className="searchBar"/>
+                <Button btnText="Add New Item"/>
             </form>
         )
     }
--- src/List/List.js
+++ src/List/List.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './List.css';
+import Form from '../Form/Form';
 
 class List extends Component {
     render(){
         return(
             <div className="listContainer">
                 <h1>• Shopping List •</h1>
+                <Form />
             </div>
         )
     }
