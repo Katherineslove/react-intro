9ddc7b7b3d1aba8c6973fda117b2a94e8cf8cc0b
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-29 15:09:35 +1300

    commented out the es5 way

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -3,10 +3,30 @@ import './Form.css';
 import Button from '../Button/Button';
 
 class Form extends Component {
+
+    // constructor(props){
+    //     super(props);
+    //
+    //     this.changeInput = this.changeInput.bind(this);
+    // }
+    //
+    // changeInput(e){
+    //     console.log(e.target.value);
+    // }
+
+    changeInput = (e) => {
+        console.log(e.target.value);
+    }
+
+
+    submitForm = (e) => {
+        e.preventDefault();
+    }
+
     render(){
         return(
-            <form className="searchForm">
-                <input className="searchBar"/>
+            <form className="searchForm" onSubmit={this.submitForm}>
+                <input onChange={this.changeInput} value={this.props.item.value} className="searchBar"/>
                 <Button btnText="Add New Item"/>
             </form>
         )
--- src/List/List.js
+++ src/List/List.js
@@ -4,15 +4,53 @@ import Form from '../Form/Form';
 import Item from '../Item/Item';
 
 class List extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            allItems : [
+                {
+                    id: 1,
+                    text: 'Apples'
+                },
+                {
+                    id: 2,
+                    text: 'Peaches'
+                },
+                {
+                    id: 3,
+                    text: 'Beers'
+                },
+                {
+                    id: 4,
+                    text: 'Sausages'
+                }
+            ],
+            currentItem: {
+                id: null,
+                value: ''
+            }
+        }
+    }
+
     render(){
+        console.log(this.state.allItems);
         return(
             <div className="listContainer">
-                <h1>• Shopping List •</h1>
-                <Form />
-                <div className="list">
-                    <Item />
-                    <Item />
-                </div>
+            <h1>• Shopping List •</h1>
+            <Form
+                item={this.state.currentItem}
+            />
+            <div className="list">
+            {
+                this.state.allItems.map(singleItem => {
+                    return <Item
+                    key = {singleItem.id}
+                    itemInfo= {singleItem}
+                    />
+                })
+            }
+            </div>
             </div>
         )
     }
